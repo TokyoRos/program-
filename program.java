ea33bd71a36aefdaecb6753066fdde83f6c57450
TokyoRos <119948769+TokyoRos@users.noreply.github.com>
2023-01-12 23:24:53 -0600

    Create program.java

---
+++ program.java
@@ -0,0 +1,28 @@
+package myprject;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		// this prints in upper class
+		System.out.print("Wait\n".toLowerCase());
+		
+		// this prints in lower case
+		System.out.print("patiently\n".toUpperCase());
+		
+		// this adds a tab
+		System.out.print("\tand the\n");
+		
+		// this left justifies two springs
+		System.out.format("%-10s%-10s", "fruit", "will fall");
+		System.out.print("\n");
+		
+		// this right justifies
+		System.out.format("%10s", "into your hand.\n");
+		
+		System.out.println(" \n");
+		// this print normally
+		System.out.print("Wait patiently and the fruit will fall into your hand.\n");
+	}
+
+}
